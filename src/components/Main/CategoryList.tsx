41ffeeb1d04a72cbb40bc993164d768636597c68
ingong <insong1216@gmail.com>
2021-10-03 21:16:24 +0900

    feat: 게시글 목록 컴포넌트

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -23,6 +23,12 @@ const CategoryListWrapper = styled.div`
   flex-wrap: wrap;
   width: 768px;
   margin: 100px auto 0;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 50px;
+    padding: 0 20px;
+  }
 `;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -32,10 +38,14 @@ const CategoryItem = styled(({ active, to, ...props }: GatsbyLinkProps) => (
   margin-right: 20px;
   padding: 5px 0;
   font-size: 18px;
-  font-weight: ${({ active }) => (active ? 'bold' : 'normal')}
+  font-weight: ${({ active }) => (active ? 'bold' : 'normal')};
   cursor: pointer;
   &:last-of-type {
     margin-right: 0;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 15px;
   }
 `;
 
@@ -43,8 +53,6 @@ const CategoryList: FunctionComponent<CategoryListProps> = function ({
   selectedCategory,
   categoryList,
 }) {
-  console.log(categoryList);
-  console.log(selectedCategory);
   return (
     <CategoryListWrapper>
       {Object.entries(categoryList).map(([name, count]) => (
